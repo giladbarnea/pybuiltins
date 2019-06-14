5b4187b6e1ed595c519feaddffd6919b6f85d205
Gilad Barnea <giladbrn@gmail.com>
2019-06-14 23:43:16 +0300

    int passes 3 nested for loops with weird prefixes

--- src/test/int.spec.ts
+++ src/test/int.spec.ts
@@ -2,10 +2,31 @@ import {int} from "../int";
 import {performance} from "perf_hooks"
 import {range} from "../range"
 
+const L = [
+    ['0', 0],
+    ['1', 1],
+    ['9', 9],
+    ['10', 10],
+    ['99', 99],
+    ['100', 100],
+    ['314', 314],
+    [' 314', 314],
+    ['314 ', 314],
+    ['  \t\t  314  \t\t  ', 314],
+    // [repr(sys.maxsize), sys.maxsize],
+    // ['  1x', ValueError],
+    ['  1  ', 1],
+    // ['  1\02  ', ValueError],
+    // ['', ValueError],
+    // [' ', ValueError],
+    // ['  \t\t  ', ValueError],
+    // ["\u0200", ValueError]
+];
 /**\Lib\test\test_int.py*/
 test('test_basic', () => {
     expect(int(314)).toEqual(314);
     expect(int(3.14)).toEqual(3);
+    // Check that conversion from float truncates towards zero
     expect(int(-3.14)).toEqual(-3);
     expect(int(3.9)).toEqual(3);
     expect(int(-3.9)).toEqual(-3);
@@ -13,9 +34,31 @@ test('test_basic', () => {
     expect(int(-3.5)).toEqual(-3);
     expect(int("-3")).toEqual(-3);
     expect(int(" -3 ")).toEqual(-3);
+    // Different base:
     expect(int("10", 16)).toEqual(16);
+    // Test conversion from strings and various anomalies
+    for (let [s, v] of L) {
+        for (let sign of ["", "+", "-"]) {
+            for (let prefix of ["", " ", "\t", "  \t\t  "]) {
+                let ss = prefix + sign + s;
+                let vv = v;
+                if (sign == "-"
+                    // && v !== ValueError
+                ) {
+                    vv = -v
+                }
+                try {
+                    expect(int(ss)).toEqual(vv)
+                } catch (e) {
+                    // if (!(e instanceof ValueError)) {
+                    // }
+                    
+                }
+            }
+        }
+    }
 });
-const start = performance.now();
+/*const start = performance.now();
 console.log({start});
 for (let i of range(1000000)) {
     int(i)
@@ -23,5 +66,6 @@ for (let i of range(1000000)) {
 const end = performance.now();
 console.log({end});
 console.log('end - start', end - start);
+*/
 
 
