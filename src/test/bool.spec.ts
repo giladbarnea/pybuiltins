549ea62b2df0f24cd60cd24696eff281a1b21fd2
Gilad Barnea <giladbrn@gmail.com>
2019-06-14 13:53:47 +0300

    added math and subclass tests for bool.

--- src/test/bool.spec.ts
+++ src/test/bool.spec.ts
@@ -1,5 +1,23 @@
 import {bool} from "../bool";
+import {int} from "../int";
 
+test('subclass', () => {
+    expect(() => {
+        class C extends bool {
+            pass
+        }
+    }).toThrow(TypeError)
+});
+test('math', () => {
+    // /Lib/test/test_bool.py.test_math:106
+    for (let b of [bool(false), bool(true)]) {
+        for (let i of [0, 1, 2]) {
+            expect(b ** i).not.toBe(int(b) ** i);
+            expect(b ** i).toEqual(int(b) ** i);
+        }
+    }
+    
+});
 describe('empty list', () => {
     it('should be falsey when passed an empty list', () => {
         const actual = bool([]);
