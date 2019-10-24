7e1461663f51790c753dbae94d027be8834e9d54
giladbarnea <giladbrn@gmail.com>
2019-10-24 19:28:47 +0300

    24-10-19 1928

--- src/enumerate.ts
+++ src/enumerate.ts
@@ -1,11 +1,23 @@
-import {int} from "./int";
-
-export function enumerate<T>(collection: T[] | T): [string[] | number[] | T[]] {
-    let entries = Object.entries(collection);
-    // @ts-ignore
-    entries.map(entry => entry[0] = entry[0].isdigit()
-        ? int(entry[0])
-        : entry[0]);
-    // @ts-ignore
-    return entries;
+function enumerate(obj: undefined): [void];
+function enumerate(obj: null): never;
+function enumerate<T>(obj: T[]): [number, T][];
+function enumerate<T>(obj: T): [keyof T, T[keyof T]][];
+function enumerate(obj) {
+    if (obj === undefined)
+        return [];
+    if (obj === null)
+        throw new TypeError('null is not iterable');
+    let array = [];
+    if (Array.isArray(obj) || typeof obj[Symbol.iterator] === 'function') {
+        let i: number = 0;
+        for (let x of obj) {
+            array.push([i, x]);
+            i++;
+        }
+    } else {
+        for (let prop in obj) {
+            array.push([prop, obj[prop]]);
+        }
+    }
+    return array;
 }
